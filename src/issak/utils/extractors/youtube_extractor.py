ab6d31cd2ac87f1903277e3e03b160d7563fe9f0
Almas Issakov <almasm4pro@Almass-MacBook-Pro.local>
2025-04-18 15:27:26 +0500

    cookies fix

--- src/issak/utils/extractors/youtube_extractor.py
+++ src/issak/utils/extractors/youtube_extractor.py
@@ -1,19 +1,35 @@
+import os
+import shutil
 import yt_dlp
 from youtube_transcript_api import YouTubeTranscriptApi
-import os
 
 def get_youtube_transcript(video_url: str) -> dict:
     try:
+        env_cookie_path = os.getenv('YOUTUBE_COOKIES_FILE')
+        default_cookie_path = 'cookies.txt'  
+        source_cookie_path = env_cookie_path if env_cookie_path else default_cookie_path
+
+        tmp_cookie_path = '/tmp/cookies.txt'
+        if not os.path.exists(tmp_cookie_path):
+            try:
+                shutil.copy(source_cookie_path, tmp_cookie_path)
+            except PermissionError:
+                return {"error": "Cannot copy cookies.txt to writable path (/tmp). Check permissions."}
+            except FileNotFoundError:
+                return {"error": f"Cookie file not found at {source_cookie_path}"}
+
+        # 3. yt-dlp с указанием куки-файла
         ydl_opts = {
             'quiet': True,
-            'cookiefile': os.getenv('YOUTUBE_COOKIES_FILE')
+            'cookiefile': tmp_cookie_path,
         }
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(video_url, download=False)
 
         video_id = info['id']
-        title = info['title']
-        thumbnail_url = info['thumbnail']
+        title = info.get('title', '')
+        thumbnail_url = info.get('thumbnail', '')
 
         try:
             transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'ru'])
@@ -28,6 +44,6 @@ def get_youtube_transcript(video_url: str) -> dict:
         }
 
     except yt_dlp.utils.DownloadError as e:
-        return {"error": f"Error while fetching video: {str(e)}"}
+        return {"error": f"yt-dlp error: {str(e)}"}
     except Exception as e:
         return {"error": str(e)}
